e19d927da2b37a95c76aacc61503daf7936a2e66
Maxim Sivak <qxblvde@gmail.com>
2024-12-07 20:12:51 +0300

    Update unit_tests.py

--- unit_tests.py
+++ unit_tests.py
@@ -1,76 +1,73 @@
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
 import unittest
 import circle
 import rectangle
 import square
 import triangle
 
-class CircleTestCase(unittest.TestCase):
+class TestCircle(unittest.TestCase):
     def test_zero_area(self):
-        res = area(0)
+        res = circle.area(0)
         self.assertEqual(res, 0)
-       
+
     def test_area(self):
-        res = area(5)
+        res = circle.area(5)
         self.assertAlmostEqual(res, 78.53981633974483, places=7)
 
     def test_perimeter(self):
-        res = perimeter(5)
+        res = circle.perimeter(5)
         self.assertAlmostEqual(res, 31.41592653589793, places=7)
-        
+
     def test_zero_perimeter(self):
-        res = perimeter(0)
+        res = circle.perimeter(0)
         self.assertEqual(res, 0)
-        
-class RectangleTestCase(unittest.TestCase):
+
+class TestRectangle(unittest.TestCase):
     def test_zero_area(self):
-        res = area(10, 0) 
+        res = rectangle.area(10, 0)
         self.assertEqual(res, 0)
-       
-    def test_square_area(self):
-        res = area(10, 10)  
+
+    def test_area(self):
+        res = rectangle.area(10, 10)
         self.assertEqual(res, 100)
 
-    def test_square_perimeter(self):
-        res = perimeter(15, 10)
+    def test_perimeter(self):
+        res = rectangle.perimeter(15, 10)
         self.assertEqual(res, 50)
 
     def test_zero_perimeter(self):
-        res = perimeter(0, 0)
+        res = rectangle.perimeter(0, 0)
         self.assertEqual(res, 0)
-        
-class SquareTestCase(unittest.TestCase):
+
+class TestSquare(unittest.TestCase):
     def test_zero_area(self):
-        res = area(0)
+        res = square.area(0)
         self.assertEqual(res, 0)
-       
-    def test_square_area(self):
-        res = area(15)
+
+    def test_area(self):
+        res = square.area(15)
         self.assertEqual(res, 225)
 
     def test_perimeter(self):
-        res = perimeter(5)
+        res = square.perimeter(5)
         self.assertEqual(res, 20)
-        
+
     def test_zero_perimeter(self):
-        res = perimeter(0)
+        res = square.perimeter(0)
         self.assertEqual(res, 0)
 
-class TriangleTestCase(unittest.TestCase):
+class TestTriangle(unittest.TestCase):
     def test_zero_area(self):
-        res = area(0, 5)
+        res = triangle.area(0, 5)
         self.assertEqual(res, 0)
-       
+
     def test_area(self):
-        res = area(15, 2)
+        res = triangle.area(15, 2)
         self.assertEqual(res, 15)
 
     def test_perimeter(self):
-        res = perimeter(5, 5, 6)
+        res = triangle.perimeter(5, 5, 6)
         self.assertEqual(res, 16)
-        
+
     def test_zero_perimeter(self):
-        res = perimeter(0, 0, 0)
+        res = triangle.perimeter(0, 0, 0)
         self.assertEqual(res, 0)
